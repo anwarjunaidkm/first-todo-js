205601e150c4e9fd8b0daf8914da7b6b3390405f
anwarjunaidkm <anverjunaidvilayil@gmail.com>
2022-08-25 12:09:22 +0530

    compleet first section

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ function additems()
    let kitchenData = kitchenInput.value;
    //dom elemnt--------------
    let li = document.createElement('li');
-   li.innerText = kitchenData;
+   let spanEl =document.createElement('span');
+   li.appendChild(spanEl);
+   spanEl.innerText=kitchenData
+  
    dataArray.push(kitchenData);
 //    console.log(dataArray[1]);
    console.log(dataArray);
@@ -25,6 +28,13 @@ function additems()
     trashBtn.classList.add('fas','fa-trash');
     li.appendChild(trashBtn);
     console.log(trashBtn);
+
+    //---------------crete editbtn----------
+    let editbtn = document.createElement('i');
+    editbtn.classList.add('fas','fa-edit');
+    li.appendChild(editbtn);
+    console.log(editbtn);
+
    
     
 
@@ -33,14 +43,39 @@ function additems()
  //---------------..----- deleteee function---------------
 
 function deleteItem(event){
-    console.log(event.target.classList[0]);
-     if(event.target.classList[0] === "fas"){
+    console.log(event.target.classList[1]);
+     if(event.target.classList[1] === "fa-trash"){
         let item = event.target.parentElement;
-        item.remove();
+        console.log(item);
+        //animation for deleteeee
+        item.classList.add("slideOut");
+        item.addEventListener("transitionend",function(){
+         console.log("animation completd");
+          item.remove();
+        });
+         
         
      }
 
 }
+////////////////edit item--------fun-----
+function editItem(event){
+   console.log(event.target.classList[1]);
+   if(event.target.classList[1] ==="fa-edit"){
+      let editedValue =prompt("enter update");
+      console.log(editedValue);
+      let item=event.target.parentElement;
+      let spanEl=item.querySelector('span');
+      // console.log(spanEl);
+      spanEl.innerText=editedValue;
+
+
+      
+   }
+
+}
+
+
 
 
 
@@ -53,3 +88,5 @@ document.querySelector("form").addEventListener("submit", function(event){
     additems()
   });
   kitchenItemList.addEventListener('click',deleteItem);
+  kitchenItemList.addEventListener('click',editItem);
+
